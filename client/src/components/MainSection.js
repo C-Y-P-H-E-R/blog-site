f6b719f2696973cbe4e229327fda29c64f120802
Kushagra sharma <myselflks@yahoo.com>
2021-08-06 22:58:58 +0530

    added-routing

--- client/src/components/MainSection.js
+++ client/src/components/MainSection.js
@@ -1,24 +1,29 @@
 import React from 'react'
 import './css/MainSection.css'
-import RecentArt from './RecentArt'
 import AllAbout from './AllAbout'
+import RecentArt from './RecentArt'
+import { Link, Route, BrowserRouter as Router , Switch } from 'react-router-dom'
 
 function MainSection() {
     return (
-        <div className="mainsection">
-                <div className="mainsection__recents">
-                    <div className="sub__navbar">
-                        <h4>Following</h4>
-                        <h4>Recommended</h4>
+            
+                <div className="mainsection">
+                    <div className="mainsection__recents">
+                        <Router>
+                            <div className="sub__navbar">
+                                <Link className = "subnav__styling" style={{"textDecoration": "none", "color": "black"}} to="/following"><h4>Following</h4></Link>
+                                <Link className = "subnav__styling" style={{"textDecoration": "none", "color": "black"}} to="/recommended"><h4>Recommended</h4></Link>
+                            </div>
+                            <Switch>
+                                <Route exact path="/recommended">
+                                    <RecentArt />
+                                </Route>
+                            </Switch>
+                        </Router>
                     </div>
-                    <RecentArt />
-                    <RecentArt />
-                    <RecentArt />
-                    <RecentArt />
-                    <RecentArt />
+                    <AllAbout />
                 </div>
-                <AllAbout />
-        </div>
+            
     )
 }
 export default MainSection
